87f71d099ac70e6cd5bcf869eada4f9e642fb89e
Marlinggi <146319728+Marlinggi@users.noreply.github.com>
2023-12-08 15:40:08 +0700

    Add files via upload

---
+++ Tipedata.php
@@ -0,0 +1,14 @@
+<?php
+
+$nrp ="173040888";
+$nama = "pengetahuan itu sendiri";
+$umur = 20;
+$Nilai = 75.75;
+
+echo "Nrp : ". $nrp. "</br>";
+echo "Nama : $nama</br>";
+print "umur: ".$umur; print "</br>";
+// menampilkan bilangan real dengan format 3 digit dibelakang koma printf ("Nilai : %.3f</br>", Snilai);
+printf("Nilai :%.3f<br>",$nilai);
+
+?>
